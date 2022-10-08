c308b66f37fce5f124c3bd427cae15a3195c1144
Gonzalo Casas <casas@arch.ethz.ch>
2022-10-08 16:16:08 +0200

    add console

--- src/compas_invocations/console.py
+++ src/compas_invocations/console.py
@@ -1,14 +1,67 @@
+"""
+********************************************************************************
+console
+********************************************************************************
+
+Text console UI helpers and patterns, e.g. ‘Y/n’ prompts and the like.
+
+.. currentmodule:: compas_invocations.console
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    confirm
+
+"""
+
 import sys
 
+# NOTE: originally taken from invocations https://github.com/pyinvoke/invocations/blob/main/invocations/console.py
+def confirm(question, assume_yes=True):
+    """
+    Ask user a yes/no question and return their response as a boolean.
 
-def confirm(question):
+    ``question`` should be a simple, grammatically complete question such as
+    "Do you wish to continue?", and will have a string similar to ``" [Y/n] "``
+    appended automatically. This function will *not* append a question mark for
+    you.
+    By default, when the user presses Enter without typing anything, "yes" is
+    assumed. This can be changed by specifying ``assume_yes=False``.
+
+    .. note::
+
+        If the user does not supply input that is (case-insensitively) equal to
+        "y", "yes", "n" or "no", they will be re-prompted until they do.
+
+    Parameters
+    ----------
+    question : str
+        The question part of the prompt.
+    assume_yes : bool
+        Whether to assume the affirmative answer by default. Defaults to ``True``.
+
+    Returns
+    -------
+    bool
+    """
+    if assume_yes:
+        suffix = "Y/n"
+    else:
+        suffix = "y/N"
+
     while True:
-        response = input(question).lower().strip()
+        response = input("{} [{}] ".format(question, suffix))
+        response = response.lower().strip()
 
-        if not response or response in ("n", "no"):
-            return False
+        if not response:
+            return assume_yes
 
-        if response in ("y", "yes"):
+        if response in ["y", "yes"]:
             return True
 
-        print("Focus, kid! It is either (y)es or (n)o", file=sys.stderr)
+        if response in ["n", "no"]:
+            return False
+
+        err = "Focus, kid! It is either (y)es or (n)o"
+        print(err, file=sys.stderr)
