e3c124667cf70ee2af03f735e6e0f014963dfcc4
Gonzalo Casas <casas@arch.ethz.ch>
2023-01-11 23:25:09 +0100

    Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name="compas_invocations",
-    version="0.3.2",
+    version="0.3.3",
     description="A collection of reusable pyinvoke tasks",
     long_description=long_description,
     long_description_content_type="text/markdown",
